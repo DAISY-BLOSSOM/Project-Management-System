2fe554b0b1664976ef88507bff5c8013b705ba17
DAISY-BLOSSOM <nourhan.mi115@gmail.com>
2022-03-18 20:22:43 +0200

    PMS

---
+++ src/project_sys/Project_sys.java
@@ -0,0 +1,22 @@
+
+package project_sys;
+
+import gui.login;
+
+/**
+ *
+ * @author Nourhan
+ */
+public class Project_sys {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        gui.login first=new login();
+        first.show_login();
+        
+    }
+    
+}
